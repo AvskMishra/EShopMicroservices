9eead9c55fb00cd3e8b249d6eae46bebdaa1108c
Abhishek Kumar <akkr0077@gmail.com>
2024-06-24 00:50:24 +0530

    Developed Catalog.API Infra, Handler and Endpoint Classes for CRUD

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,11 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
+﻿
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -21,8 +20,9 @@ namespace Catalog.API.Products.CreateProduct
             };
 
             //TODO:SAVE TO DB
-
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new CreateProductResult(product.Id);
         }
     }
 }
