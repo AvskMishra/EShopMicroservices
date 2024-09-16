caa99be1747566f2e52544a102d9ffb0a11c21e6
Abhishek Kumar <akkr0077@gmail.com>
2024-09-16 12:17:29 +0530

    ✨Added Discount Grpc Microservice API

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public static class Extentions
+{
+    /// <summary>
+    /// Seed and auto migrate databse using enitity framework core in discount
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
