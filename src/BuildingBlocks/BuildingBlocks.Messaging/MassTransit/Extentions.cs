244e3b7abe0eb6b686e642d8b2c123942b62b1d0
Abhishek Kumar <akkr0077@gmail.com>
2024-12-30 12:51:36 +0530

    ✨Added Messaging Queue Implementation on Building Block

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using MassTransit;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+public static class Extentions
+{
+    public static IServiceCollection AddMessageBroker
+        (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]);
+                    host.Password(configuration["MessageBroker:Password"]);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
